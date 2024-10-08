d62d9a8114bd605164b8c237c41fad605a47dcf1
11344 <11344@16S235>
2024-10-08 14:39:47 +0200

    Implements Admin Menu features

--- UserControls/Admin_Menu_UserControl.xaml.cs
+++ UserControls/Admin_Menu_UserControl.xaml.cs
@@ -20,9 +20,35 @@ namespace Electronic_journal.UserControls
     /// </summary>
     public partial class Admin_MainMenu_UserControl : UserControl
     {
-        public Admin_MainMenu_UserControl()
+        MainWindow window;
+
+        public Admin_MainMenu_UserControl(MainWindow window)
         {
             InitializeComponent();
+            this.window = window;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string userName = ((Button)sender).Name.ToString();
+            switch (userName)
+            {
+                case "classes_button":
+                    window.frame.NavigationService.Navigate(new Admin_ClassesMenu_UserControl(window));
+                    break;
+                case "teachers_button":
+                    window.frame.NavigationService.Navigate(new Admin_TeachersMenu_UserControl(window));
+                    break;
+                case "students_button":
+                    window.frame.NavigationService.Navigate(new Admin_TeachersMenu_UserControl(window));
+                    break;
+                case "logout_button":
+                    window.frame.NavigationService.GoBack();
+                    break;
+                default:
+                    Console.WriteLine("RoleSelction_UserControl - Button Error");
+                    break;
+            }
         }
     }
 }

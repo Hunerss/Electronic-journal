6a82c167d900deaadf6325596ebb14fcb5247f64
Hunerss <107001861+Hunerss@users.noreply.github.com>
2024-10-18 19:59:07 +0200

    Fixes serious issue with naming

---
+++ UserControls/AdminUserControls/Admin_PersonelMenu_UserControl.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Electronic_journal.UserControls.Admin
+{
+    /// <summary>
+    /// Logika interakcji dla klasy Admin_PersonelMenu_UserControl.xaml
+    /// </summary>
+    public partial class Admin_PersonelMenu_UserControl : UserControl
+    {
+        public Admin_PersonelMenu_UserControl()
+        {
+            InitializeComponent();
+        }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+        }
+    }
+}

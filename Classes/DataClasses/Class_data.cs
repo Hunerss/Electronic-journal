234d8d063e639e4bff2216c727e7a6b0e7cf95ab
Hunerss <107001861+Hunerss@users.noreply.github.com>
2024-10-21 20:50:16 +0200

    Updates Class_data class to match database

--- Classes/DataClasses/Class_data.cs
+++ Classes/DataClasses/Class_data.cs
@@ -8,5 +8,10 @@ namespace Electronic_journal.Classes.DataClasses
 {
     internal class Class_data
     {
+        public string Class_name {  get; set; }
+        public int Classroom { get; set; }
+        public string HomeroomTeacher_name { get; set;}
+        public string HomeroomTeacher_surname { get; set;}
+        public int Students_number { get; set; }
     }
 }

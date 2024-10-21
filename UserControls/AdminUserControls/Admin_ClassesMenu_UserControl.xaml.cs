4bef373f1ccbbd58002692e8b757078e48383969
Hunerss <107001861+Hunerss@users.noreply.github.com>
2024-10-21 20:51:28 +0200

    Adds ClassesMenu functionality

--- UserControls/AdminUserControls/Admin_ClassesMenu_UserControl.xaml.cs
+++ UserControls/AdminUserControls/Admin_ClassesMenu_UserControl.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Electronic_journal.Classes;
+using Electronic_journal.Classes.DataClasses;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,25 +25,57 @@ namespace Electronic_journal.UserControls.AdminUserControls
     {
         MainWindow window;
 
+        List<Class_data> classes;
+
         public Admin_ClassesMenu_UserControl(MainWindow window)
         {
             InitializeComponent();
             this.window = window;
         }
 
-        private void Add_menuItem_Click(object sender, RoutedEventArgs e)
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
         {
-
+            try
+            {
+                classes = DatabaseOperator.GetClasses();
+                classes_DataGrid.ItemsSource = classes;
+                SetupDataGridColumns();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error loading students: {ex.Message}");
+            }
         }
 
-        private void Remove_menuItem_Click(object sender, RoutedEventArgs e)
+        private DataGridTextColumn CreateTextColumn(string header, string bindingPath)
         {
-
+            return new DataGridTextColumn
+            {
+                Header = header,
+                Binding = new Binding(bindingPath)
+            };
         }
 
-        private void Modify_menuItem_Click(object sender, RoutedEventArgs e)
+        private void SetupDataGridColumns()
         {
+            classes_DataGrid.Columns.Clear();
 
+            classes_DataGrid.Columns.Add(CreateTextColumn("Class", "Class_name"));
+            classes_DataGrid.Columns.Add(CreateTextColumn("Classroom", "Classroom"));
+            classes_DataGrid.Columns.Add(CreateTextColumn("Homeroom Teacher Name", "HomeroomTeacher_name"));
+            classes_DataGrid.Columns.Add(CreateTextColumn("Homeroom Teacher Surname", "HomeroomTeacher_surname"));
+            classes_DataGrid.Columns.Add(CreateTextColumn("Students Count", "Students_number"));
+        }
+
+        private void menuItem_Click(object sender, RoutedEventArgs e)
+        {
+            //UpdateDatabase();
+        }
+
+        private void Return_Button_Click(object sender, RoutedEventArgs e)
+        {
+            //UpdateDatabase();
+            window.frame.NavigationService.GoBack();
         }
     }
 }

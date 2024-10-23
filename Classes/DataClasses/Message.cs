501131fc431c8c95bfa41c27584dacc068f95b7c
Hunerss <107001861+Hunerss@users.noreply.github.com>
2024-10-23 23:29:32 +0200

    Reorganizes classes

--- Classes/DataClasses/Message.cs
+++ Classes/DataClasses/Message.cs
@@ -1,6 +1,6 @@
 ﻿namespace Electronic_journal.Classes.DataClasses
 {
-    public class Message : Base
+    public class Message : Note
     {
         public bool Single_target { get; set; }
         public string Group_id { get; set; }
--- Classes/DataClasses/Note.cs
+++ Classes/DataClasses/Note.cs
@@ -2,6 +2,11 @@
 {
     public class Note : Base
     {
+        public int Id { get; set; }
+        public int Author_id { get; set; }
+        public int Target_id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
         public int Date { get; set; }
         public int Hour { get; set; }
     }

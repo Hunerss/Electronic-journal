1ce66d56e2691aafb2004669c961f95b378f55ff
Hunerss <107001861+Hunerss@users.noreply.github.com>
2024-10-23 21:55:09 +0200

    Adds and implements Teacher profile functionality

---
+++ UserControls/TeacherUserControls/Teacher_Profile_UserControl.xaml.cs
@@ -0,0 +1,68 @@
+﻿using Electronic_journal.Classes;
+using Electronic_journal.Classes.DataClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Electronic_journal.UserControls.TeacherUserControls
+{
+    /// <summary>
+    /// Logika interakcji dla klasy Teacher_Profile_UserControl.xaml
+    /// </summary>
+    public partial class Teacher_Profile_UserControl : UserControl
+    {
+        MainWindow window;
+        Teacher teacher;
+
+        public Teacher_Profile_UserControl(MainWindow window, Teacher teacher)
+        {
+            InitializeComponent();
+            this.window = window;
+            this.teacher = teacher;
+        }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            name_TextBlock.Text = teacher.Name + " " + teacher.Surname;
+            subject_TextBlock.Text = teacher.Subject;
+            homeroom_class_TextBlock.Text = teacher.Class_name.ToUpper();
+            classroom_TextBlock.Text = teacher.Classroom.ToString();
+            birthday_TextBlock.Text = FormatDate(teacher.Birthday);
+            age_TextBlock.Text = teacher.Age.ToString();
+            if (teacher.Sex == 0)
+                sex_TextBlock.Text = "Woman";
+            else
+                sex_TextBlock.Text = "Man";
+            lessons_TextBlock.Text = DatabaseOperator.GetLessonsCount(teacher.Id).ToString();
+        }
+
+        public static string FormatDate(int dateAsInt)
+        {
+            string dateAsString = dateAsInt.ToString();
+
+            int year = int.Parse(dateAsString[..4]);
+            int month = int.Parse(dateAsString.Substring(4, 2));
+            int day = int.Parse(dateAsString.Substring(6, 2));
+
+            DateTime date = new(year, month, day);
+
+            return date.ToString("dd.MM.yyyy");
+        }
+
+        private void Return_Button_Click(object sender, RoutedEventArgs e)
+        {
+            window.frame.NavigationService.GoBack();
+        }
+    }
+}

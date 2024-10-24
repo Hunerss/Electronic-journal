50377dd79696f14333289cd6e16b29179b2102fc
Hunerss <107001861+Hunerss@users.noreply.github.com>
2024-10-24 23:02:07 +0200

    Updates class

--- Classes/DataClasses/Message.cs
+++ Classes/DataClasses/Message.cs
@@ -4,7 +4,7 @@
     {
         public int School_role {  get; set; }
         public bool Single_target { get; set; }
-        public int Tareg_school_role {  get; set; }
+        public int Target_school_role {  get; set; }
         public string Group_id { get; set; }
     }
 }

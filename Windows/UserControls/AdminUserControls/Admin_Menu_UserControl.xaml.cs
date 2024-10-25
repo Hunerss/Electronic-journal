6371216747d40f89f624df73ed71c76abeb25170
Hunerss <107001861+Hunerss@users.noreply.github.com>
2024-10-25 22:13:01 +0200

    Adds and finalizes admin messages

--- Windows/UserControls/AdminUserControls/Admin_Menu_UserControl.xaml.cs
+++ Windows/UserControls/AdminUserControls/Admin_Menu_UserControl.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using Electronic_journal.Classes.DataClasses;
+using Electronic_journal.UserControls.GeneralUserControls;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace Electronic_journal.UserControls.AdminUserControls
@@ -9,11 +11,13 @@ namespace Electronic_journal.UserControls.AdminUserControls
     public partial class Admin_Menu_UserControl : UserControl
     {
         MainWindow window;
+        Admin admin;
 
-        public Admin_Menu_UserControl(MainWindow window)
+        public Admin_Menu_UserControl(MainWindow window, Admin admin)
         {
             InitializeComponent();
             this.window = window;
+            this.admin = admin;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -21,6 +25,9 @@ namespace Electronic_journal.UserControls.AdminUserControls
             string userName = ((Button)sender).Name.ToString();
             switch (userName)
             {
+                case "messages_button":
+                    window.frame.NavigationService.Navigate(new General_Messages_UserControl(window, admin));
+                    break;
                 case "personel_button":
                     window.frame.NavigationService.Navigate(new Admin_PersonelMenu_UserControl(window));
                     break;

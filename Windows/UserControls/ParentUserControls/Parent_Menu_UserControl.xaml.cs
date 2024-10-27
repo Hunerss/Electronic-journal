0a7939665fbe01ef7e16e4d857afe8d6864683cd
Hunerss <107001861+Hunerss@users.noreply.github.com>
2024-10-27 14:40:24 +0100

    Updates ParentMenu

--- Windows/UserControls/ParentUserControls/Parent_Menu_UserControl.xaml.cs
+++ Windows/UserControls/ParentUserControls/Parent_Menu_UserControl.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using Electronic_journal.Classes.DataClasses;
+using Electronic_journal.UserControls.GeneralUserControls;
+using Electronic_journal.Windows.UserControls.GeneralUserControls;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -29,6 +31,34 @@ namespace Electronic_journal.UserControls.ParentUserControls
             InitializeComponent();
             this.window = window;
             this.parent = parent;
+        }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            parent_header.Text = "Welcome - " + parent.Name + " " + parent.Surname;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string userName = ((Button)sender).Name.ToString();
+            switch (userName)
+            {
+                case "lessons_button":
+                    window.frame.NavigationService.Navigate(new General_Lessons_UserControl(window, parent));
+                    break;
+                case "messages_button":
+                    window.frame.NavigationService.Navigate(new General_Messages_UserControl(window, parent));
+                    break;
+                case "grades_button":
+                    //window.frame.NavigationService.Navigate(new Admin_LessonsMenu_UserControl(window));
+                    break;
+                case "logout_button":
+                    window.frame.NavigationService.GoBack();
+                    break;
+                default:
+                    Console.WriteLine("RoleSelction_UserControl - Button Error");
+                    break;
+            }
         }
     }
 }

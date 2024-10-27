2f025ebf18473f2b74c553cb8b57667b54f3547a
Hunerss <107001861+Hunerss@users.noreply.github.com>
2024-10-27 13:51:01 +0100

    Creates grade giving feature base

---
+++ Windows/UserControls/TeacherUserControls/Teacher_Grades_UserControl.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Electronic_journal.Windows.UserControls.TeacherUserControls
+{
+    /// <summary>
+    /// Logika interakcji dla klasy Teacher_Grades_UserControl.xaml
+    /// </summary>
+    public partial class Teacher_Grades_UserControl : UserControl
+    {
+        public Teacher_Grades_UserControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
